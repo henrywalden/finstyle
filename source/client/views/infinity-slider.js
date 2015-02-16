2e1c4aa9cae1b5bde63a5325f907c74d916a227e
Henry Walden <henry@grout.io>
2015-02-16 20:15:54 +1100

    Add infinity slider plugin as bare scripts

--- source/client/views/infinity-slider.js
+++ source/client/views/infinity-slider.js
@@ -1,38 +1,51 @@
-  infinitySlider(
-      infinitySliderId = 'slide01',
-      infinitySliderCommandsClass = 'iS-Commands',
-      infinitySliderPreviousButtonClass = 'iS-Previous',
-      infinitySliderNextButtonClass = 'iS-Next',
-      infinitySliderDotsClass = 'iS-Dots',
-      infinitySliderDotClass = 'iS-Dot',
-      infinitySliderDotActiveClass = 'iS-Dotactive',
-      infinitySliderPlayButtonClass = 'iS-Play',
-      infinitySliderPlayButtonActiveClass = 'iS-Playactive',
-      infinitySliderStopButtonClass = 'iS-Stop',
-      infinitySliderStopButtonActiveClass = 'iS-Stopactive',
-      infinitySliderLoopIndicator = 'iS-Loopline',
-      infinitySliderContentClass = 'iS-Content',
-      infinitySliderItemsClass = 'iS-Items',
-      infinitySliderItemClass = 'iS-Item',
-      infinitySliderAutoStartLoop = true,
-      infinitySliderKeyboardNavigation = true,
-      infinitySliderTouchNavigation = 'desktop',
-      infinitySliderStarterSlide = 1
-  );
+Template.sliderTest.rendered = function() {
+    infinitySlider(
+        infinitySliderId = 'slide01',
+        infinitySliderCommandsClass = 'iS-Commands',
+        infinitySliderPreviousButtonClass = 'iS-Previous',
+        infinitySliderNextButtonClass = 'iS-Next',
+        infinitySliderDotsClass = 'iS-Dots',
+        infinitySliderDotClass = 'iS-Dot',
+        infinitySliderDotActiveClass = 'iS-Dotactive',
+        infinitySliderPlayButtonClass = 'iS-Play',
+        infinitySliderPlayButtonActiveClass = 'iS-Playactive',
+        infinitySliderStopButtonClass = 'iS-Stop',
+        infinitySliderStopButtonActiveClass = 'iS-Stopactive',
+        infinitySliderLoopIndicator = 'iS-Loopline',
+        infinitySliderContentClass = 'iS-Content',
+        infinitySliderItemsClass = 'iS-Items',
+        infinitySliderItemClass = 'iS-Item',
+        infinitySliderAutoStartLoop = true,
+        infinitySliderKeyboardNavigation = true,
+        infinitySliderTouchNavigation = 'desktop',
+        infinitySliderStarterSlide = 1
+    );
 
-  if ($(window).width() > 1024) {
-    $('.numberRoll').html('0');
-    $('.numberRoll').click(function () {
-      var current = $(this);
-      if (current.hasClass('numberRoll')) {
-        numberRoll($(this), 1000, 100, 0);
-        $(this).removeClass('numberRoll');
-      }
+    if ($(window).width() > 1024) {
+        $('.numberRoll').html('0');
+        $('.numberRoll').click(function () {
+            var current = $(this);
+            if (current.hasClass('numberRoll')) {
+                numberRoll($(this), 1000, 100, 0);
+                $(this).removeClass('numberRoll');
+            }
+        });
+    }
+
+    $(window).load(function () {
+        setTimeout(function () {
+            $('.iS-Stop').click();
+        }, 91000);
     });
-  }
 
-  setTimeout(function () {
-    $('.iS-Stop').click();
-  }, 91000);
+    var fullscreen = $('.epeoFullscreen');
+    fullScreen(fullscreen);
+    $(window).bind('load resize',function(){
+        fullScreen(fullscreen);
+    });
+
+
+};
+
 
 
---
+++ source/client/views/infinity-slider/is_plus_extract.js
@@ -0,0 +1,56 @@
+                                
+                            /*************************
+jQuery Infinty Slider v.1.5 Extensions
+@Website: http://http://www.epeo.it/infinityslider/
+@Author: Epeo
+@Copyright: 09/06/2014
+************************/
+autoCenter = function(thisItems) {
+	thisItems.each(function() {
+		var thisOnly = $(this);
+		thisOnly.css({
+			left: 'auto',
+			right: 'auto',
+			marginRight: 'auto',
+			marginLeft: 'auto'
+		});
+		var thisParent = thisOnly.parent();
+		var thisMargin = (thisParent.width()-thisOnly.outerWidth())/2;
+		thisOnly.css({marginLeft: thisMargin});
+	});
+};
+
+autoMiddle = function(thisItems) {
+	thisItems.each(function() {
+		var thisOnly = $(this);
+		thisOnly.css({
+			top: 'auto',
+			bottom: 'auto',
+			marginBottom: 'auto',
+			marginTop: 'auto'
+		});
+		var thisParent = thisOnly.parent();
+		var thisMargin = (thisParent.height()-thisOnly.outerHeight())/2;
+		thisOnly.css({marginTop: thisMargin});
+	});
+};
+
+fullScreen = function(thisItems) {
+	thisItems.each(function(){
+		$(this).css({
+			width: $(window).width(),
+			height: $(window).height()
+		})
+	});
+};
+
+
+var center = $('.epeoCenter');
+var middle = $('.epeoMiddle');
+var fullscreen = $('.epeoFullscreen');
+fullScreen(fullscreen);
+$(window).bind('load resize',function(){
+	fullScreen(fullscreen);
+	//autoCenter(center);
+	//autoMiddle(middle);
+});
